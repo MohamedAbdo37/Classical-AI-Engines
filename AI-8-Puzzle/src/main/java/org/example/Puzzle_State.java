5844a0ebffb6729a07f8324215d4d9041628381a
NiraIbrahem <125669902+NiraIbrahem@users.noreply.github.com>
2024-10-22 12:08:26 +0300

    Update Puzzle_State.java

--- AI-8-Puzzle/src/main/java/org/example/Puzzle_State.java
+++ AI-8-Puzzle/src/main/java/org/example/Puzzle_State.java
@@ -1,25 +1,31 @@
 package org.example;
 
 public class Puzzle_State {
-    private int[] puzzle ;
-    private int empty_Position ;
+    private int Board;
+    private int Empty_cell_Position;
     private Puzzle_State parent ;
+    private int depth ;
 
-    public Puzzle_State(int[] puzzle , int empty_Position , Puzzle_State parent){
-        this.puzzle = puzzle ;
-        this.empty_Position = empty_Position ;
+    public Puzzle_State(int Board, int Empty_cell_Position, Puzzle_State parent, int depth){
+        this.Board = Board;
+        this.Empty_cell_Position = Empty_cell_Position;
         this.parent = parent ;
+        this.depth = depth ;
     }
 
-    public int[] getPuzzle() {
-        return this.puzzle;
+    public int getBoard() {
+        return this.Board;
     }
 
-    public int getempty_Position() {
-        return this.empty_Position;
+    public int getEmpty_cell_Position() {
+        return this.Empty_cell_Position;
     }
 
     public Puzzle_State getParent() {
         return this.parent;
+    }
+
+    public int getDepth() {
+        return depth;
     }
 }
