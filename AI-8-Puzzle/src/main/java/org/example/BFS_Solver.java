99131ae5cd1f1ceef6cabdeaa420f8a73e7d57cb
NiraIbrahem <125669902+NiraIbrahem@users.noreply.github.com>
2024-10-22 12:07:50 +0300

    Update BFS_Solver.java

--- AI-8-Puzzle/src/main/java/org/example/BFS_Solver.java
+++ AI-8-Puzzle/src/main/java/org/example/BFS_Solver.java
@@ -3,95 +3,102 @@ import java.util.* ;
 
 public class BFS_Solver {
 
-    boolean check_goal(int[] array){
-        int [] goal = new int[] {0,1,2,3,4,5,6,7,8} ;
-        return Arrays.equals(goal , array) ;
-    }
-
-    boolean check_dublication(Queue frontier , Queue explored , int[] state){
-        Iterator<Puzzle_State> puzzleIterator = frontier.iterator();
-
-        while (puzzleIterator.hasNext()) {
-            Puzzle_State next = puzzleIterator.next();
-            if(Arrays.equals(next.getPuzzle(),state)){
-                return true ;
-            }
-        }
-        puzzleIterator = explored.iterator() ;
-        while (puzzleIterator.hasNext()) {
-            Puzzle_State next = puzzleIterator.next();
-            if(Arrays.equals(next.getPuzzle(),state)){
-                return true ;
-            }
-        }
-        return false ;
-    }
-
+    private int Maximum_depth ;
+    private int expanded_nodes ;
     Puzzle_State Breadth_First_Search(Puzzle_State initialState){
+
         Queue<Puzzle_State> frontier = new LinkedList<>() ;
         Queue<Puzzle_State> explored = new LinkedList<>() ;
         frontier.add(initialState) ;
 
-        for(int i=0 ; i<9 ; i++)
-            System.out.print(initialState.getPuzzle()[i]+" ");
-        System.out.println();
+        // adding states reached before
+        HashSet<Integer> Reached_Before = new HashSet<>() ;
+        Reached_Before.add(initialState.getBoard()) ;
+
+        this.Maximum_depth = 0 ;
+        this.expanded_nodes = 0 ;
 
         while(!frontier.isEmpty()){
             Puzzle_State state = frontier.remove() ;  // the node that will be next expanded
             explored.add(state);
-
-            if(check_goal(state.getPuzzle())){  // goal is reached
+            if(state.getBoard() == 12345678){  // goal is reached
+                this.expanded_nodes = explored.size() -1 ;
                 return state ;
             }
 
-            int x = state.getempty_Position() ; // position of empty cell
+            int i = state.getEmpty_cell_Position() ; // position of empty cell
+            String x = Integer.toString(state.getBoard()) ;     // converting state to string
+            if(i==0)
+                x = "0" + x ;
 
-            if(x-3 >= 0){ // move up
-                int [] next_stage = state.getPuzzle().clone() ;
-                next_stage[x] = next_stage[x-3] ;
-                next_stage[x-3] = 0 ;   // exchange the empty cell with the up cell
+            if(i-3 >= 0){ // move up
+                String moving_up = x ;
+                char cell_up = moving_up.charAt(i-3) ;
+                moving_up = moving_up.substring(0 , i-3) + "0" + moving_up.substring(i-2 , i) + cell_up + moving_up.substring(i+1 ) ;
+                int y = Integer.parseInt(moving_up) ;
 
                 // check if the node is in frontier or explored queues
-                if(!check_dublication(frontier , explored , next_stage)){
-                    frontier.add(new Puzzle_State(next_stage , x-3 , state)) ;
+                if(!Reached_Before.contains(y)){
+                    frontier.add(new Puzzle_State(y,  i- 3, state, state.getDepth()+1 )) ;
+                    Reached_Before.add(y) ;
+                    this.Maximum_depth = state.getDepth()+1 ;
                 }
             }
 
-            if(x+3 <= 8) { // move down
-                int[] next_stage = state.getPuzzle().clone();
-                next_stage[x] = next_stage[x + 3];
-                next_stage[x + 3] = 0;// exchange the empty cell with the up cell
+            if(i+3 <= 8) { // move down
+
+                String moving_down = x ;
+                char cell_down = moving_down.charAt(i+3) ;
+                moving_down = moving_down.substring(0 , i) + cell_down + moving_down.substring(i+1 , i+3) + "0" + moving_down.substring(i+4 ) ;
+                int y = Integer.parseInt(moving_down) ;
 
                 // check if the node is in frontier or explored queues
-                if (!check_dublication(frontier, explored, next_stage)) {
-                    frontier.add(new Puzzle_State(next_stage, x + 3, state));
+                if(!Reached_Before.contains(y)){
+                    frontier.add(new Puzzle_State(y,  i+ 3, state, state.getDepth()+1 )) ;
+                    Reached_Before.add(y) ;
+                    this.Maximum_depth = state.getDepth()+1 ;
                 }
             }
 
-            if(x%3 !=0){ // move left
-                int [] next_stage = state.getPuzzle().clone() ;
-                next_stage[x] = next_stage[x-1] ;
-                next_stage[x-1] = 0 ;   // exchange the empty cell with the up cell
+            if(i%3 !=0){ // move left
+                String moving_left = x ;
+                char cell_left = moving_left.charAt(i-1) ;
+                moving_left = moving_left.substring(0 , i-1) + "0" +  cell_left + moving_left.substring(i+1) ;
+                int y = Integer.parseInt(moving_left) ;
 
                 // check if the node is in frontier or explored queues
-                if(!check_dublication(frontier , explored , next_stage)){
-                    frontier.add(new Puzzle_State(next_stage , x-1 , state)) ;
+                if(!Reached_Before.contains(y)){
+                    frontier.add(new Puzzle_State(y,  i-1 , state, state.getDepth()+1 )) ;
+                    Reached_Before.add(y) ;
+                    this.Maximum_depth = state.getDepth()+1 ;
                 }
             }
 
-            if(x %3 !=2){ // move right
-                int [] next_stage = state.getPuzzle().clone() ;
-                next_stage[x] = next_stage[x+1] ;
-                next_stage[x+1] = 0 ;   // exchange the empty cell with the up cell
+            if(i %3 !=2){ // move right
+                String moving_right = x ;
+                char cell_right = moving_right.charAt(i+1) ;
+                moving_right = moving_right.substring(0 , i)  + cell_right + "0" +  moving_right.substring(i+2 ) ;
+                int y = Integer.parseInt(moving_right) ;
 
                 // check if the node is in frontier or explored queues
-                if(!check_dublication(frontier , explored , next_stage)){
-                    frontier.add(new Puzzle_State(next_stage , x+1 , state)) ;
+                if(!Reached_Before.contains(y)){
+                    frontier.add(new Puzzle_State(y,  i+ 1 , state, state.getDepth()+1 )) ;
+                    Reached_Before.add(y) ;
+                    this.Maximum_depth = state.getDepth()+1 ;
                 }
             }
         }
 
+        this.expanded_nodes = explored.size() ;
         return null;
 
+    }
+
+    public int getMaximum_depth() {
+        return Maximum_depth;
+    }
+
+    public int getExpanded_nodes() {
+        return expanded_nodes;
     }
 }
