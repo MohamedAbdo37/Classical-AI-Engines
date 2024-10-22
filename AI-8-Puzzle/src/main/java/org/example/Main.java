302f79338da8c772de3d0240dfd927e16f82beca
NiraIbrahem <125669902+NiraIbrahem@users.noreply.github.com>
2024-10-22 12:09:48 +0300

    Update Main.java

--- AI-8-Puzzle/src/main/java/org/example/Main.java
+++ AI-8-Puzzle/src/main/java/org/example/Main.java
@@ -1,19 +1,19 @@
-package org.example;
+// package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+// public class Main {
+//     public static void main(String[] args) {
 
-        int a[] = new int [] {1,6,2,7,3,4,8,0,5};
-//        int a[] = new int [] {0,1,2,3,4,5,6,7,8};
+//         int a[] = new int [] {1,6,2,7,3,4,8,0,5};
+// //        int a[] = new int [] {0,1,2,3,4,5,6,7,8};
 
-//        int a[] = new int [] {1,2,5,3,4,0,6,7,8};
-//        int a[] = new int [] {1,4,2,6,5,8,7,3,0};
+// //        int a[] = new int [] {1,2,5,3,4,0,6,7,8};
+// //        int a[] = new int [] {1,4,2,6,5,8,7,3,0};
 
         BFS_Solver bfs = new BFS_Solver();
-        Puzzle_Solver ps = new Puzzle_Solver() ;
+        bfs.Breadth_First_Search(new Puzzle_State(125340678 , 5 , null , 0)) ;
+        System.out.println(bfs.getMaximum_depth());
+        System.out.println(bfs.getExpanded_nodes());
 
-        ps.display_solution(bfs.Breadth_First_Search(new Puzzle_State(a, ps.search_start(a) , null)));
 
-
-    }
-}
+//     }
+// }
