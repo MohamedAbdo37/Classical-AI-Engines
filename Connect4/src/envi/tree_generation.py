996b7e207c0e97a90bf5353e5ce49ba3ba4e82e2
NiraIbrahem <125669902+NiraIbrahem@users.noreply.github.com>
2024-12-03 00:49:14 +0200

    Update tree_generation.py

--- Connect4/src/envi/tree_generation.py
+++ Connect4/src/envi/tree_generation.py
@@ -51,7 +51,7 @@ class tree_generation :
 
         # generating root
         initial_state.node_name = 'root'
-        dot.node(initial_state.node_name , tree_generation.tree_node(initial_state))
+        dot.node(initial_state.node_name , tree_generation.tree_node(initial_state) , shape = 'triangle' , color = 'purple' , style = 'filled')
 
         # printing nodes level by level
         queue = [] 
@@ -61,15 +61,157 @@ class tree_generation :
             state = queue.pop()
             for i in range(len(state.children)):
                 state.children[i].node_name = 'node' + str(num)
-                dot.node(state.children[i].node_name, tree_generation.tree_node(state.children[i]))
+                if(state.depth %2 == 0) :
+                    dot.node(state.children[i].node_name, tree_generation.tree_node(state.children[i]) , shape = 'invtriangle' , color = 'orange' , style = 'filled')
+                else :
+                    dot.node(state.children[i].node_name, tree_generation.tree_node(state.children[i]) , shape = 'triangle' , color = 'purple' , style = 'filled')
+
                 dot.edge(state.node_name ,state.children[i].node_name)
                 num = num+1
                 queue.append(state.children[i])
-        
-        
+
+
         global count
         # opening file
         dot.render('D://search tree' + str(count), format='svg')    
         count = count +1   
 
         return 'D://search tree' + str(count-1) + '.svg'
+
+
+
+    def excepted_minmax_tree(initial_state) :
+        dot = graphviz.Digraph()
+        dot  
+
+        # generating root
+        initial_state.node_name = 'root'
+        dot.node(initial_state.node_name , tree_generation.tree_node(initial_state) ,shape = 'triangle' , color = 'purple' , style = 'filled')
+
+        # printing nodes level by level
+        queue = [] 
+        queue.append(initial_state)
+        num = 0
+        chance_num = 0 
+
+        while len(queue)!=0 :
+            state = queue.pop()
+
+            if state.children == [] :
+                continue 
+            
+            for col in range (7) :
+
+                if state.children[col] != None :
+
+                    if state.depth % 2 == 0 :
+                        shape = 'invtriangle' 
+                        color = 'orange'
+
+                    else :
+                        shape = 'triangle' 
+                        color = 'purple' 
+
+                    
+                    dot.node('chance' + str(chance_num), '' , shape = 'circle' , color = 'yellow' , style = 'filled')
+                    dot.edge(state.node_name , 'chance' + str(chance_num))
+                        
+                    if (state.chance_nodes[col]) == 3 :
+
+                        state.children[col-1].node_name = 'node' + str(num)
+                        dot.node(state.children[col-1].node_name, tree_generation.tree_node(state.children[col-1]) , shape = shape , color = color , style = 'filled')
+                        num = num+1
+                        dot.edge('chance' + str(chance_num) , state.children[col-1].node_name , '.2')
+
+
+                        state.children[col].node_name = 'node' + str(num)
+                        dot.node(state.children[col].node_name, tree_generation.tree_node(state.children[col]) ,  shape = shape , color = color , style = 'filled')
+                        num = num+1
+                        dot.edge('chance' + str(chance_num) , state.children[col].node_name , '.6')
+
+                        state.children[col+1].node_name = 'node' + str(num)
+                        dot.node(state.children[col+1].node_name, tree_generation.tree_node(state.children[col+1]) ,  shape = shape , color = color , style = 'filled')
+                        num = num+1
+                        dot.edge('chance' + str(chance_num) , state.children[col+1].node_name , '.2')
+
+                        queue.append(state.children[col-1].copy())
+                        queue.append(state.children[col].copy())
+                        queue.append(state.children[col+1].copy())
+
+
+                    elif (state.chance_nodes[col]) == 2 :
+                        if col ==0 :
+                            state.children[col].node_name = 'node' + str(num)
+                            dot.node(state.children[col].node_name, tree_generation.tree_node(state.children[col]) , shape = shape , color = color , style = 'filled')
+                            num = num+1
+                            dot.edge('chance' + str(chance_num) , state.children[col].node_name , '.6')
+
+                            state.children[col+1].node_name = 'node' + str(num)
+                            dot.node(state.children[col+1].node_name, tree_generation.tree_node(state.children[col+1]) ,  shape = shape , color = color , style = 'filled')
+                            num = num+1
+                            dot.edge('chance' + str(chance_num) , state.children[col+1].node_name , '.4')
+
+                            queue.append(state.children[col].copy())
+                            queue.append(state.children[col+1].copy())
+
+
+                        elif col == 6 :
+                            state.children[col-1].node_name = 'node' + str(num)
+                            dot.node(state.children[col-1].node_name, tree_generation.tree_node(state.children[col-1]) ,  shape = shape , color = color , style = 'filled')
+                            num = num+1
+                            dot.edge('chance' + str(chance_num) , state.children[col-1].node_name , '.4')
+
+
+                            state.children[col].node_name = 'node' + str(num)
+                            dot.node(state.children[col].node_name, tree_generation.tree_node(state.children[col]) ,  shape = shape , color = color , style = 'filled')
+                            num = num+1
+                            dot.edge('chance' + str(chance_num) , state.children[col].node_name , '.6')
+
+                            queue.append(state.children[col-1].copy())
+                            queue.append(state.children[col].copy())
+
+
+                        else :
+                            if state.children[col-1] == None :
+                                state.children[col].node_name = 'node' + str(num)
+                                dot.node(state.children[col].node_name, tree_generation.tree_node(state.children[col]) ,  shape = shape , color = color , style = 'filled')
+                                num = num+1
+                                dot.edge('chance' + str(chance_num) , state.children[col].node_name , '.6')
+
+                                state.children[col+1].node_name = 'node' + str(num)
+                                dot.node(state.children[col+1].node_name, tree_generation.tree_node(state.children[col+1]) ,  shape = shape , color = color , style = 'filled')
+                                num = num+1
+                                dot.edge('chance' + str(chance_num) , state.children[col+1].node_name , '.4')
+
+                                queue.append(state.children[col].copy())
+                                queue.append(state.children[col+1].copy())
+
+                            else :
+                                state.children[col-1].node_name = 'node' + str(num)
+                                dot.node(state.children[col-1].node_name, tree_generation.tree_node(state.children[col-1]) ,  shape = shape , color = color , style = 'filled')
+                                num = num+1
+                                dot.edge('chance' + str(chance_num) , state.children[col-1].node_name , '.4')
+
+                                state.children[col].node_name = 'node' + str(num)
+                                dot.node(state.children[col].node_name, tree_generation.tree_node(state.children[col]) ,  shape = shape , color = color , style = 'filled')
+                                num = num+1
+                                dot.edge('chance' + str(chance_num) , state.children[col].node_name , '.6')
+
+                                queue.append(state.children[col-1].copy())
+                                queue.append(state.children[col].copy())
+
+                    else :
+                        state.children[col].node_name = 'node' + str(num)
+                        dot.node(state.children[col].node_name, tree_generation.tree_node(state.children[col]) ,  shape = shape , color = color , style = 'filled')
+                        num = num+1
+                        dot.edge('chance' + str(chance_num) , state.children[col].node_name , '1')
+
+                        queue.append(state.children[col].copy())
+                    
+                    chance_num = chance_num + 1
+
+        global count
+        # opening file
+        dot.render('D://search tree ' + str(count), format='svg')    
+        count = count +1   
+        return 'D://search tree ' + str(count-1) + '.svg'
